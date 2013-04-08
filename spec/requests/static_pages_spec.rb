682a261b726d63ba65fab8298fd54e089c8207a5
zzz6519003 <zhengzhongzhao@gmail.com>
2013-04-08 10:37:29 +0800

    in the middle of chapter 4.4

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,15 +1,22 @@
 require 'spec_helper'
 
 describe "Static pages" do
+      # describe "Contact page" do
+      #       it "should have the right title" do
+      #             visit '/static_pages/contact'
+      #             page.should have_selector('h1', :text => "Contact")
 	describe "Home page" do
             it "should have the h1 'Sample App'" do
                   visit '/static_pages/home'
                   page.should have_selector('h1', :text => 'Sample App')
             end
-            it "should have the title 'Home'" do
+            it "should have the base title 'Home'" do
                   visit '/static_pages/home'
-                  page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Home")
+                  page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App")
             end
+            it "should not have a custom page title" do
+                  visit '/static_pages/home'
+                  page.should_not have_selector('title', :text => '| Home')
       end
 
       describe "Help page" do
